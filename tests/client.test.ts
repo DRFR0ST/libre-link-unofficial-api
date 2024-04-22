71e18190cb5687713bcb2f37f464b82979fc1f6e
Mike Eling <mike.eling97@gmail.com>
2024-04-23 00:47:40 +0200

    :white_check_mark: Add unit tests for patientId

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -3,7 +3,7 @@ import { LibreLinkClient } from '../src';
 import { mapObjectPropertiesToTypes } from "./utils";
 
 describe('LibreLinkClient', () => {
-  let client: LibreLinkClient = new LibreLinkClient();;
+  const client: LibreLinkClient = new LibreLinkClient();
 
   test('should be created', () => {
     expect(client).toBeTruthy();
@@ -28,5 +28,24 @@ describe('LibreLinkClient', () => {
 
     expect(data).toBeTruthy();
     expect(mapObjectPropertiesToTypes(data!)).toMatchSnapshot();
+  });
+
+  test('should initialize with a patientId', async () => {
+    const customClient = new LibreLinkClient({ patientId: "7f51ab27-c7c8-11ed-bcc3-0242ac110002" });
+
+    await customClient.login();
+
+    expect(customClient.me).toBeTruthy();
+  });
+
+  test('should throw error with an invalid patientId', async () => {
+    const customClient = new LibreLinkClient({ patientId: "invalid-patient-id" });
+
+    try {
+      await customClient.login();
+    } catch(err) {
+      expect(err).toBeTruthy();
+      expect(err.message).toMatch(/(Patient ID not found in connections. (invalid-patient-id))/i);
+    }
   });
 });
