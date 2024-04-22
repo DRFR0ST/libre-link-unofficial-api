c0bd175790bd37c5133e0f0eaf5798ef636120af
Mike Eling <mike.eling97@gmail.com>
2024-04-23 00:06:33 +0200

    :white_check_mark: Add unit tests

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,14 +1,32 @@
 import { expect, test, beforeEach, describe } from "bun:test";
 import { LibreLinkClient } from '../src';
+import { mapObjectPropertiesToTypes } from "./utils";
 
 describe('LibreLinkClient', () => {
-  let client: LibreLinkClient;
-
-  beforeEach(() => {
-    client = new LibreLinkClient();
-  });
+  let client: LibreLinkClient = new LibreLinkClient();;
 
   test('should be created', () => {
     expect(client).toBeTruthy();
+  });
+
+  test('should successfully login', async () => {
+    await client.login();
+
+    expect(client.me).toBeTruthy();
+    expect(mapObjectPropertiesToTypes(client.me)).toMatchSnapshot();
+  });
+
+  test('should successfully fetch connections', async () => {
+    const { data } = await client.fetchConnections();
+
+    expect(data).toBeTruthy();
+    expect(mapObjectPropertiesToTypes(data!)).toMatchSnapshot();
+  });
+
+  test('should successfully read data', async () => {
+    const { data } = await client.read();
+
+    expect(data).toBeTruthy();
+    expect(mapObjectPropertiesToTypes(data!)).toMatchSnapshot();
   });
 });
---
+++ tests/utils.ts
@@ -0,0 +1,31 @@
+interface TypeMap {
+    [key: string]: string | TypeMap; // Allow nested TypeMaps for objects
+}
+
+/**
+ * @description Map object properties to types. Mainly used for snapshotting objects to check for changes.
+ */
+export const mapObjectPropertiesToTypes = (obj: Record<string, any>): TypeMap => {
+    const typeMap: TypeMap = {};
+
+    for (const key in obj) {
+        const value = obj[key];
+        const type = typeof value; 
+
+        if (type === 'object') {
+            if (Array.isArray(value)) { 
+                typeMap[key] = 'array'; // Indicate an array
+            } else if (value === null) {
+                typeMap[key] = 'null'; // Handle null
+            } else {
+                // Recurse for nested objects
+                typeMap[key] = mapObjectPropertiesToTypes(value); 
+            }
+        } else {
+            // Primitive type
+            typeMap[key] = type;  
+        }
+    }
+
+    return typeMap;
+}
